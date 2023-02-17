8ae0e311ecd0b013175f3fe060c00207bc9e58d9
chisom david <chisomdavid840@gmail.com>
2023-02-17 04:27:52 +0000

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
   */
 int main(void)
 {
-fputs(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
 return (1);
 }
