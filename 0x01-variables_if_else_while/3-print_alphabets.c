72e8804528a65a595b3aa92d5a1d4aa55a441a24
chisom david <chisomdavid840@gmail.com>
2023-02-17 22:28:41 +0000

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  *main - this is the entry point
+  *Return: this ends the program
+  */
+int main(void)
+{
+	char string1[] = "abcdefghijklmnopqrstuvwxyz";
+	char string2[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (int i = 0; string1[i] != '\0'; i++)
+	{
+		putchar(string1[i]);
+	}
+
+	
+
+	for (int i = 0; string2[i] != '\0'; i++)
+	{
+		putchar(string2[i]);
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
