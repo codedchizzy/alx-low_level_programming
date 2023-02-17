62c4d3ede6c5db66e58927e55db0ffddddf13c73
chisom david <chisomdavid840@gmail.com>
2023-02-17 02:30:49 +0000

    101

---
+++ 0x00-hello_world/101-qoutes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  *main - is the entry point
+  *fprintf - print the string
+  *Return: ends the file withthe value 1
+  */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is usefu
+			l\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
