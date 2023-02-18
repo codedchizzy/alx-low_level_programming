a3e6aa9e8d59e466a0b7bbe9d2dc1a9bf663d4fc
chisom david <chisomdavid840@gmail.com>
2023-02-18 00:05:07 +0000

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - entry point
+  *Return: Ends program with 0
+  */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%d\n",num);
+		num++;
+	}
+	return (0);
+}
