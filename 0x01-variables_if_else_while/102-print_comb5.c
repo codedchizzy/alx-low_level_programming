a446259f59b8b5967e70c72903dd531aaa34804d
chisom david <chisomdavid840@gmail.com>
2023-02-21 10:41:16 +0000

    102.02

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,19 +9,22 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 98; a++)
+	for (a = 0; a < 100; a++)
 	{
-		for (b = 0; b <= 99; b++)
+		for (b = 0; b < 100; b++)
 		{
-			putchar((a / 10) + '0');
-			putchar((a % 10) + '0');
-			putchar(' ');
-			putchar((b / 10) + '0');
-			putchar((b % 10) + '0');
-			if (a < 98 || b < 99)
+			if (a < b)
 			{
-				putchar(',');
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
 				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
