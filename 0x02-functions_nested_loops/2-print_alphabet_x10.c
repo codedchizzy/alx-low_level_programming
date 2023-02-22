3368db1cb7b31c31eb5ae1980e00a69b99f3d896
chisom david <chisomdavid840@gmail.com>
2023-02-22 02:22:30 +0000

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  *print_alphabet_x10 - print a-z x10
+  */
+
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		int x;
+
+		for (x = 0; x < 10; x++)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n')
+}
