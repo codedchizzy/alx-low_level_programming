15132a2ca22f02f48a6c163f53ce796722b6b6ae
chisom david <chisomdavid840@gmail.com>
2023-02-22 03:17:07 +0000

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *print_sign - check for equality
+  *@n: character used
+  *Return: ends the program
+  */
+
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar(+);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(-);
+		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
+	}
+}
