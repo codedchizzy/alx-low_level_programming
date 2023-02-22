ead3ccd5b19c1b05186aaf5b66e6f5ba2d36cb8f
chisom david <chisomdavid840@gmail.com>
2023-02-22 18:46:07 +0000

    0.2

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -14,8 +14,10 @@
 void positive_or_negative(int i)
 {
 
-	srand(time(0));
-	i = rand() - RAND_MAX / 2;
+	/**
+	  *srand(time(0));
+	  *i = rand() - RAND_MAX / 2;
+	  */
 	/* your code goes there */
 	if (i > 0)
 	{
