a8acbbb0068a4f18c2b744c3a652a83c98365dc2
chisom david <chisomdavid840@gmail.com>
2023-02-22 03:47:31 +0000

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+/**
+  *print_last_digit - print last digit
+  *@n: the character the we want to prints it last digit
+  *Return: end the program
+  */
+
+int print_last_digit(int n)
+{
+	int l = n % 10;
+
+	_putchar(l);
+	return (0);
+}
