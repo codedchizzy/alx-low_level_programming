a2d11166d39431db63efc54a20bec4f7ba5cbdcd
chisom david <chisomdavid840@gmail.com>
2023-02-22 04:31:08 +0000

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "min.h"
+#include <stdio.h>
+/**
+  *add - sum of two numbers
+  *return end the progrm
+  */
+
+int add(int, int)
+{
+	int a, b, s;
+
+	int s = a + b;
+	_putchar(s + '1')
+	return (0);
+}
