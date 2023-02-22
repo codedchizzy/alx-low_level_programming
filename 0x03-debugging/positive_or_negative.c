2ae1f2233a5965f6aba74c6de04e696cf9e1c361
chisom david <chisomdavid840@gmail.com>
2023-02-22 18:24:23 +0000

    0.1

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,24 +9,22 @@
   *srand - generate random numbers
   *Return: endsthe program with valye of 0
   */
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	i = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
+	if (i > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", i);
 	}
-	else if (n < 0)
+	else if (i < 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	}
 	else
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	}
-	return (0);
 }
