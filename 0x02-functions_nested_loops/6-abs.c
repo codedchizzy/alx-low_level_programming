2537a3efd82b666a3d3789892374f0b5c0c5a7c5
chisom david <chisomdavid840@gmail.com>
2023-02-22 03:37:18 +0000

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+/**
+  *_abs - computes the absolute value of an integer
+  *@e: the interer to compute absoutw value
+  *Return: the absolue value of n
+  */
+int _abs(int e)
+{
+	if (e < 0)
+	{
+		return (-e);
+	}
+	else
+	{
+		return (e);
+	}
+}
