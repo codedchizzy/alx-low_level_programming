5ef386a9b623681ae8ee886d4c0c1e2badd8164b
chisom david <chisomdavid840@gmail.com>
2023-02-23 22:45:35 +0000

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}
