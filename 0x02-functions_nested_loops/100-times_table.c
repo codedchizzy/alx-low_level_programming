66892ad2b84a72bbb33120a6fd086b67586bdbb5
chisom david <chisomdavid840@gmail.com>
2023-02-23 03:07:38 +0000

    100.1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,9 +9,9 @@ void print_times_table(int n)
 {
 	int a, b, c;
 
-	for (a = 0; a < n; a++)
+	for (a = 0; a <= n; a++)
 	{
-		for (b = 0; b < n; b++)
+		for (b = 0; b <= n; b++)
 		{
 			c = b * a;
 			if (b == 0)
