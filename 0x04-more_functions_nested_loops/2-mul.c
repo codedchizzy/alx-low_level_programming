e40fcd923e8a77be189964860036d17d25ba0e4f
chisom david <chisomdavid840@gmail.com>
2023-02-23 21:52:12 +0000

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.c"
+
+/**
+ * mul - multiplies 2 integers
+ * @a: a character
+ * @b: character
+ * Return: gets the vakue and do the operation
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
