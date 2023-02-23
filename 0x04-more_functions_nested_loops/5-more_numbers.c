a73d32d07bc6814211a9934a1ed6c963018bd76f
chisom david <chisomdavid840@gmail.com>
2023-02-23 23:50:21 +0000

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			_putchar(num + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
