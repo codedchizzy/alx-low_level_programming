137d111e9983d5116bad535226ded6fc274c37ed
chisom david <chisomdavid840@gmail.com>
2023-02-24 02:49:47 +0000

    8

--- 0x04-more_functions_nested_loops/8-main.c
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -3,7 +3,10 @@
 int main(void)
 {
 	print_square(2);
-	print_square(10);	  
-	print_square(0);
+	print_square(30);	  
+	print_square(10);
+	print_square(5);
+	print_square(20);
+
 	return (0);
 }
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,7 +9,7 @@ void print_square(int size)
 {
 	int a, b;
 
-	if(size <= 0)
+	if (size <= 0)
 		_putchar('\n');
 	else
 		for (a = 1; a <= size; a++)
