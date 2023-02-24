38bb264ca773773e0d62e40a3c7a3260117de989
chisom david <chisomdavid840@gmail.com>
2023-02-24 12:16:46 +0000

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,27 +7,36 @@
 
 int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 1; i <= 100; i++)
+	for (num = 1; num <= 100; num++)
+
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
+
+	if (num % 3 == 0 && num % 5 == 0)
+
+	printf("FizzBuzz ");
+
+	else if (num % 3 == 0 && num % 5 != 0)
+
+	printf("Fizz ");
+
+	else if (num == 100)
+
+	printf("Buzz");
+
+	else if (num % 5 == 0 && num % 3 != 0)
+
+	printf("Buzz ");
+
+	else
+
+	printf("%d ", num);
+
 	}
+
 	printf("\n");
+
 	return (0);
+
 }
