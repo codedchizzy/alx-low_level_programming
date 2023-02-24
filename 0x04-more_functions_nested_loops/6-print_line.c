f03ece31b079bfd9f8579d12a74a099992a27d25
chisom david <chisomdavid840@gmail.com>
2023-02-24 00:35:42 +0000

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  *print_line - print line n long
+  *@n: hwhh
+  */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a <= n; a++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar(_);
+		}
+	}
+	_putchar('\n');
+}
