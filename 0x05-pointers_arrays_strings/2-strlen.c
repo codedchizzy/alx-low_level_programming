8f36398c6b7e4bc497fac3fb2de2dc08c1cb557b
chisom david <chisomdavid840@gmail.com>
2023-02-27 23:37:56 +0000

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - get the legnth of string
+ *@s: string
+ *Return: returns i
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	return (i);
+}
