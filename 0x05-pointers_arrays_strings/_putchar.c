27723c32b76a69ca293a443e77c1624ed18a5b90
chisom david <chisomdavid840@gmail.com>
2023-02-27 23:02:09 +0000

    1

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _putchar - acts as putchar
+ * Return: return putchar
+ */
+
+int _putchar(int c)
+{
+	return putchar(c);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <string.h>
+
+int _putchar(int c);
+void reset_to_98(int *n);
+
+#endif 
