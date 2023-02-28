7171249d5430c08425593321fb04c9441c9a934e
chisom david <chisomdavid840@gmail.com>
2023-02-28 03:37:57 +0000

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,19 +7,25 @@
 
 void puts_half(char *str)
 {
-	int i, l, n;
-
-	l = 0;
-
-	for (i = 0; str[i] != '\0'; i++)
-		l++;
-
-	n = (l / 2);
-
-	if ((l % 2) == 1)
-		n = ((l + 1) / 2);
-
-	for (i = n; str[i] != '\0'; i++)
-		_putchar(str[i]);
+	int len, n, i;
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
 	_putchar('\n');
 }
