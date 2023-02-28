344f15311bf0bb8b898b517bc8f9ee8acb368f93
chisom david <chisomdavid840@gmail.com>
2023-02-28 01:23:17 +0000

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,18 +2,12 @@
 
 void print_rev(char *s)
 {
-	int length = _strlen(s);
+	int count = 0;
+	int i, n;
 
-	int i;
-
-	for (i = length - 1; i >= 0; i--)
-	{
-		if (s[i] == ' ')
-		{
-			s[i] = '\0';
-
-			_puts(&(s[i]) + 1);
-		}
-	}
-	_puts(s);
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (n = (count - 1); n >= 0; n--)
+		_putchar(s[n]);
+	_putchar('\n');
 }
