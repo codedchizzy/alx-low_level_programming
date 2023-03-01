91a00ce6db904617c151afd9a64610d95f344b29
chisom david <chisomdavid840@gmail.com>
2023-03-01 22:32:32 +0000

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *reverse_array - rev
+  *@a: char
+  *@n: char
+  */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
