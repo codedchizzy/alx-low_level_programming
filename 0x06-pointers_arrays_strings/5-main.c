d9e38d91c96dcfd87b939704a85dd60d6ebf1384
chisom david <chisomdavid840@gmail.com>
2023-03-01 22:55:26 +0000

    5

---
+++ 0x06-pointers_arrays_strings/5-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int main(void)
+{
+	    char str[] = "Look up!\n";
+	        char *ptr;
+
+		    ptr = string_toupper(str);
+		        printf("%s", ptr);
+			    printf("%s", str);
+			        return (0);
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+  *string_toupper - to upper
+  *@ : jjsh
+  *Return: return str
+  */
+
+char *string_toupper(char *str)
+{
+	char *p = str;
+
+	while (*p != '\0')
+	{
+		if (islower(*p))
+		{
+			*p = toupper(*p);
+		}
+		p++;
+	}
+	return (str);
+}
