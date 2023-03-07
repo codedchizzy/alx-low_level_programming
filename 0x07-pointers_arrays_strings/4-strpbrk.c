cbd87b76fdee59359348d594ef2aca67cf033add
chisom david <chisomdavid840@gmail.com>
2023-03-07 00:59:41 +0000

    4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *_strpbrk - jdhd
+  *@s: jshs
+  *@accept: jjj
+  *Return: s
+  */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
