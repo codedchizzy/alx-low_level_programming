9cf590a1de73beccb37979e580b6cd01294a5474
chisom david <chisomdavid840@gmail.com>
2023-03-08 17:40:01 +0000

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,7 +14,7 @@ int _pow_recursion(int x, int y)
 	else if (y == 1)
 		return (x);
 	else if (y < 0)
-		return (1 / _pow_recursion(x, -y));
+		return (-1);
 	else
 		return (x * _pow_recursion(x, y - 1));
 }
