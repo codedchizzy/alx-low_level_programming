79833953be5210fc042ec735bfe48891e44820d7
chisom <chisomdavid840@gmail.com>
2023-03-16 18:10:56 -0400

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,9 +16,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, k;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = 0;
 	if (s2 == NULL)
-		s2 = "";
+		s2 = 0;
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{
