5e52da729accff218bd5adcbf792c996976c7380
chisom <chisomdavid840@gmail.com>
2023-03-16 22:44:17 -0400

    2. _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - ....
+ * @nmemb: ....
+ * @size: ....
+ * Return: .......
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+	{
+		*((char*)ptr + 1) = 0;
+	}
+	return (ptr);
+}
