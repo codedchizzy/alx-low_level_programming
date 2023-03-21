7d04a493d86eddbc32955592abe53abb75fed172
chisom <chisomdavid840@gmail.com>
2023-03-21 16:59:41 -0400

    added doc to file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include "dog.h"
 
+/**
+ * init_dog - initojj
+ * @d: dee
+ * struct dog - dog
+ * @name: name
+ * @age: age
+ * @owner: help
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d -> name = name;
-	d -> age = age;
-	d -> owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
