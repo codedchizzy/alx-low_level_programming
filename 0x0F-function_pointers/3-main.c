71f0f0a58c4f35c2a610bccd9eedf91c53248667
chisom <chisomdavid840@gmail.com>
2023-03-23 09:00:09 -0400

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "3-calc.h"
 
 /**
@@ -10,36 +9,35 @@
  *
  * Return: 0 if successful, otherwise an error code.
  */
-
 int main(int argc, char **argv)
 {
-	int num1, num2, result;
-	int (*op_func)(int, int);
+    int num1, num2, result;
+    int (*op_func)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		return (98);
-	}
+    if (argc != 4)
+    {
+        printf("Error\n");
+        return (98);
+    }
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	op_func = get_op_func(argv[2]);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+    op_func = get_op_func(argv[2]);
 
-	if (op_func == NULL)
-	{
-		printf("Error\n");
-		return (99);
-	}
+    if (op_func == NULL)
+    {
+        printf("Error\n");
+        return (99);
+    }
 
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-	{
-		printf("Error\n");
-		return (100);
-	}
+    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        return (100);
+    }
 
-	result = op_func(num1, num2);
-	printf("%d\n", result);
+    result = op_func(num1, num2);
+    printf("%d\n", result);
 
-	return (0);
+    return (0);
 }
