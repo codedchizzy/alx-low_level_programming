0a0568a4a45256e9495206022a0e9ff20f965e84
chisom <chisomdavid840@gmail.com>
2023-03-23 09:01:53 -0400

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,41 +3,42 @@
 #include "3-calc.h"
 
 /**
- * main - Entry point.
- * @argc: The number of arguments passed to the program.
- * @argv: An array of strings containing the arguments passed to the program.
- *
- * Return: 0 if successful, otherwise an error code.
+ * main - Start here to do stuff
+ * @argc: Number of arguments passed in
+ * @argv: Arguments stored in each index
+ * Return: Success
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*op_func)(int, int);
+	int result, num1, num2;
+	int (*ptr)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (argc < 4 || argv[1] == NULL || argv[3] == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    op_func = get_op_func(argv[2]);
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    if (op_func == NULL)
-    {
-        printf("Error\n");
-        return (99);
-    }
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        return (100);
-    }
+	ptr = get_op_func(argv[2]);
 
-    result = op_func(num1, num2);
-    printf("%d\n", result);
+	if (ptr == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    return (0);
+	result = ptr(num1, num2);
+
+	printf("%d\n", result);
+
+	return (0);
 }
