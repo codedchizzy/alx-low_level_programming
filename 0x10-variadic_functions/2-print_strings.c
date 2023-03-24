1349e99c4843bad845d18875c284e72fc97325a0
chisom <chisomdavid840@gmail.com>
2023-03-24 10:00:02 -0400

    debugging

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,9 +12,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	char *str;
-
 	va_list arg;
+
 	va_start(arg, n);
+
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(arg, char*);
